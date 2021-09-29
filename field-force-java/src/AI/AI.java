721c7f32d16a0b2edfe9d7c84e0e1d43aaee85e5
ksburhan <burhan.koeseler@hotmail.de>
2021-09-29 17:47:30 +0200

    Last update

--- field-force-java/src/AI/AI.java
+++ field-force-java/src/AI/AI.java
@@ -68,21 +68,20 @@ public class AI {
         checkTimelimit();
         boolean maximizer;
         maximizer = currentplayer.getPlayerNumber() == GameConstants.OWN_PLAYER_ID;
-        if(depth == 0 || gameState.isGameOver())
+        if (depth == 0 || gameState.isGameOver())
             return Evaluator.evaluate(gameState);
 
-        if(maximizer) {
+        if (maximizer) {
             float maxScore = Float.MIN_VALUE;
             List<Move> moves = gameState.getAllMoves(currentplayer.getPlayerNumber());
-            for(Move m : moves){
+            for (Move m : moves) {
                 GameState copy = new GameState(gameState);
                 copy.simulateNextGamestate(currentplayer.getPlayerNumber(), m);
 
                 float rating = minmax(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
                 maxScore = Math.max(maxScore, rating);
                 alpha = Math.max(alpha, rating);
-                if(beta <= alpha){
-                    System.out.println("cutoff");
+                if (beta <= alpha) {
                     break;
                 }
             }
@@ -90,106 +89,21 @@ public class AI {
         } else {
             float minScore = Integer.MAX_VALUE;
             List<Move> moves = gameState.getAllMoves(currentplayer.getPlayerNumber());
-            for(Move m : moves){
+            for (Move m : moves) {
                 GameState copy = new GameState(gameState);
                 copy.simulateNextGamestate(currentplayer.getPlayerNumber(), m);
 
                 float rating = minmax(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
                 minScore = Math.min(minScore, rating);
                 beta = Math.min(beta, rating);
-                if(beta < alpha){
-                    System.out.println("cutoff");
+                if (beta < alpha) {
                     break;
                 }
             }
             return minScore;
         }
-
-        /*for (Move m : moves){
-            GameState copy = new GameState(gameState);
-            copy.simulateNextGamestate(currentplayer.getPlayerNumber(), m);
-            rating = minmax(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
-            if(maximizer){
-                if (rating > alpha){
-                    alpha = rating;
-                    if(depth == currentMaxDepth){
-                        bestMove = m;
-                    }
-                }
-                if(alpha >= beta) {
-                    return alpha;
-                }
-            } else {
-                if(rating <= beta){
-                    beta = rating;
-                    if(alpha >= beta){
-                        return beta;
-                    }
-                }
-            }
-        }
-        if(maximizer)
-            return alpha;
-        else
-            return beta;*/
-    }
-/*
-    private int maximizer(int depth, int alpha, int beta, Player player, GameState gameState){
-        try {
-            checkTimelimit();
-        } catch (TimeoutException e) {
-            return alpha;
-        }
-        if(depth == 0)
-            return Evaluator.evaluate(gameState);
-        List<Move> moves = gameState.getAllMoves(player.getPlayerNumber());
-
-        for (Move m : moves){
-            GameState copy = new GameState(gameState);
-            copy.simulateNextGamestate(player.getPlayerNumber(), m);
-            int rating = minimizer(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
-            if(rating > alpha){
-                alpha = rating;
-                if(alpha == 150)
-                    return alpha;
-            }
-            if(alpha >= beta)
-                return alpha;
-=======
-            if(!Constants.VERBOSE) System.out.println("Time is running out");
-=======
-            if(!GameConstants.VERBOSE) System.out.println("Time is running out");
->>>>>>> 112b9fe8a8e992fd03843436fc255b51ff850578
-            return bestMove;
->>>>>>> 69bd98a06daab227fe50001fab833ca73a8b63f5
-        }
-        return alpha;
     }
 
-    private int minimizer(int depth, int alpha, int beta, Player player, GameState gameState){
-        if(depth == 0)
-            return Evaluator.evaluate(gameState);
-        List<Move> moves = gameState.getAllMoves(player.getPlayerNumber());
-
-        for (Move m : moves){
-            GameState copy = new GameState(gameState);
-            copy.simulateNextGamestate(player.getPlayerNumber(), m);
-            int rating;
-            if(copy.getNextPlayer().getPlayerNumber() == AI.ownPlayerID)
-                rating = maximizer(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
-            else
-                rating = minimizer(depth - 1, alpha, beta, copy.getNextPlayer(), copy);
-            if(rating <= beta){
-                beta = rating;
-                if(beta == 150)
-                    return beta;
-            }
-            if(alpha >= beta)
-                return beta;
-        }
-        return beta;
-    }
-*/
     private void checkTimelimit() throws TimeoutException {
         if (System.currentTimeMillis() - AI.time_start > GameConstants.TIME_LIMIT - 500) {
             throw new TimeoutException();
